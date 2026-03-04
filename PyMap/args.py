e257ff60c79f1151241dabf649da98f439c6ba15
D43eM0N <kaanaral68@gmail.com>
2026-03-04 12:48:44 +0300

    Update args.py

--- PyMap/args.py
+++ PyMap/args.py
@@ -3,19 +3,42 @@ from PingSweep.exceptionHandling import validate_ip_address
 
 
 def _args():
-    parser = argparse.ArgumentParser(usage="<module> -n <net> -d <ip> -s", description="PyMap is a tool for basic networking stuff. Which could able to do ip and port scan " \
-    "or many kind of specific jobs!", 
-    epilog="You can give a feedback on my github which my nickname is D43emM0N! Thanks for having me!")
+    parser = argparse.ArgumentParser(usage="-d <ip> -n <net> -s", 
+                                     
+    description="PyMap: Network Scanning Tool", 
 
-    parser.add_argument("-d", "--device", metavar="device ip", help="Enter your devices ip", required=True)
-    parser.add_argument("-n", "--net", metavar="net type", help="Define network type")
-    parser.add_argument("-s", "--scan", action='store_true' ,help="Scans the LAN network")
-    parser.add_argument("-i", "--interface",action="store_true" ,help="Shows Network Information about device")
-    parser.add_argument("-p", "--port", metavar="port")
+    epilog="You can give a feedback on my github which my nickname is D43emM0N! Thanks for having me!"
+
+    ,add_help=True)
+
+
+    parser.add_argument("-d", "--device", 
+                        metavar="device ip", 
+                        help="Target IP address (e.g., 192.168.1.1)", 
+                        required=True
+                        )
+    
+    parser.add_argument("-n", "--net", 
+                        metavar="net type", 
+                        help="Define network type"
+                        )
+    
+    parser.add_argument("-s", "--scan", 
+                        action='store_true' ,
+                        help="Scans the LAN network"
+                        )
+    
+    parser.add_argument("-i", "--interface",
+                        action="store_true" ,
+                        help="Shows Network Information about device"
+                        )
 
     args = parser.parse_args()
 
-    validate_ip_address(args.device)
+    if args.device in ["-h", "--help"]:
+        print(parser.print_help())
+    else:
+        validate_ip_address(args.device)
     
-    
+
     return args
