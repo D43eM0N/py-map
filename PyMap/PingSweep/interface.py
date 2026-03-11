df73ae0540f77fc905b74ab3aaecd3e77d9a0155
d43m0n <kaanaral68@gmail.com>
2026-03-11 14:06:58 +0300

    Delete PyMap directory

--- PyMap/PingSweep/interface.py
+++
@@ -1,9 +0,0 @@
-import socket
-import ipaddress
-
-
-def get_net_info(ip: str, mask: str) -> str:
-    host = socket.gethostname()
-    net = ipaddress.IPv4Network(f"{ip}/{mask}", strict=False)
-
-    return host,net,f"/{net.prefixlen}"
